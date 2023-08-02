b7a404b34e6c95a940afa6a71d115259e5b4cf08
Lucas Faria <lucasff15@hotmail.com>
2023-08-02 00:00:50 -0300

    feat: about page desktop

--- client/src/components/about/AboutContent.tsx
+++ client/src/components/about/AboutContent.tsx
@@ -2,19 +2,51 @@ import { styled } from '@mui/material/styles';
 import aboutArt from '/assets/imgs/aboutArt.png';
 
 const StyledAboutContent = styled('section')(() => ({
+  width: '100%',
   '& img': {
     marginBottom: '3rem',
   },
+  '@media(min-width: 700px)': {
+    width: '100%',
+    paddingBottom: '6.5rem',
+    marginBlock: '100px',
+    '& .about-image': {
+      width: '50%',
+      scale: '1.1',
+      float: 'right',
+      shapeOutside:
+        'polygon(469px 479px, 44.9% 85.03%, 20.44% 79.70%, 19.37% 24.62%)',
+      marginLeft: '5rem',
+      marginBottom: '2rem',
+      position: 'relative',
+      top: '-6vmin',
+    },
+  },
 }));
 const AboutContentWrapper = styled('section')(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column-reverse',
   padding: '3rem 1rem 0 1rem',
+  width: '100%',
+  '@media(min-width: 700px)': {
+    display: 'block',
+    width: '100%',
+    maxwidth: '1080px',
+  },
+
   '& h2': {
-    fontSize: '40px',
+    fontSize: 'clamp(40px, 10vw, 50px)',
     color: theme.palette.primary.light,
     marginBottom: '1.5rem',
+    '@media(min-width: 700px)': {
+      marginBottom: '2.35rem',
+      marginTop: '2.35rem',
+    },
   },
   '& p': {
-    fontSize: '20px',
+    fontSize: 'clamp(20px, 3vw, 22px)',
+    lineHeight: '35px',
+    letterSpacing: '.5px',
     marginBottom: '1rem',
     '&:last-child': {
       marginBottom: 0,
@@ -24,31 +56,34 @@ const AboutContentWrapper = styled('section')(({ theme }) => ({
 
 export default function AboutContent() {
   return (
-    <StyledAboutContent>
+    <StyledAboutContent className='spacing'>
       <AboutContentWrapper>
-        <h2>Sobre Nós</h2>
-        <p>
-          Nossa missão é simplificar e tornar compreensível o processo de
-          aplicação para o seu visto. Aqui, você encontrará todas as informações
-          necessárias para compreender o funcionamento do processo e receber
-          orientações detalhadas sobre o preenchimento dos formulários.
-        </p>
-        <p>
-          Além disso, oferecemos um guia passo a passo para que você saiba
-          exatamente o que fazer e quais documentos apresentar para solicitar
-          seu visto de forma independente, caso opte por esse caminho.
-        </p>
-        <p>
-          Estamos comprometidos em auxiliá-lo em cada etapa do processo,
-          garantindo que sua experiência seja a mais tranquila e bem-sucedida
-          possível.
-        </p>
-        <p>
-          Dê as boas-vindas ao Maple Visa Guide e conte conosco para tornar seu
-          sonho de viajar para o exterior uma realidade!
-        </p>
+        <img className='about-image' src={aboutArt} alt='canada art' />
+        <div>
+          <h2>Sobre Nós</h2>
+          <p>
+            Nossa missão é simplificar e tornar compreensível o processo de
+            aplicação para o seu visto. Aqui, você encontrará todas as
+            informações necessárias para compreender o funcionamento do processo
+            e receber orientações detalhadas sobre o preenchimento dos
+            formulários.
+          </p>
+          <p>
+            Além disso, oferecemos um guia passo a passo para que você saiba
+            exatamente o que fazer e quais documentos apresentar para solicitar
+            seu visto de forma independente, caso opte por esse caminho.
+          </p>
+          <p>
+            Estamos comprometidos em auxiliá-lo em cada etapa do processo,
+            garantindo que sua experiência seja a mais tranquila e bem-sucedida
+            possível.
+          </p>
+          <p>
+            Dê as boas-vindas ao Maple Visa Guide e conte conosco para tornar
+            seu sonho de viajar para o exterior uma realidade!
+          </p>
+        </div>
       </AboutContentWrapper>
-      <img src={aboutArt} alt='canada art' />
     </StyledAboutContent>
   );
 }
--- client/src/components/about/AboutHero.tsx
+++ client/src/components/about/AboutHero.tsx
@@ -10,23 +10,36 @@ const StyledHero = styled('section')(() => ({
   color: '#fff',
 }));
 const StyledHeroTitle = styled('section')(() => ({
-  width: '90%',
+  width: '100%',
   paddingTop: '90px',
+  '@media (min-width: 700px)': {
+    paddingTop: '0',
+  },
   '& h1': {
-    fontSize: '32px',
+    fontSize: 'clamp(32px, 5vw, 48px)',
     marginBottom: '1rem',
   },
   '& p': {
-    fontSize: '16px',
+    fontSize: 'clamp(16px, 2vw, 20px)',
+  },
+  '& .about-wrapper': {
+    width: '80%',
+    maxWidth: '1080px',
+    '@media (min-width: 800px)': {
+      width: '45%',
+      minWidth: '360px',
+    },
   },
 }));
 
 export default function AboutHero() {
   return (
     <StyledHero>
-      <StyledHeroTitle>
-        <h1>Bem-Vindo ao Maple Visa Guide!</h1>
-        <p>Seu Especialista na Imigração para o Canadá e os EUA!</p>
+      <StyledHeroTitle className='spacing'>
+        <div className='about-wrapper'>
+          <h1>Bem-Vindo ao Maple Visa Guide!</h1>
+          <p>Seu Especialista na Imigração para o Canadá e os EUA!</p>
+        </div>
       </StyledHeroTitle>
     </StyledHero>
   );
