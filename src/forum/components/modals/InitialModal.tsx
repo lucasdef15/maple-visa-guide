3dd81a2ca37434629f36a355d6675d4d489b4447
Lucas Faria <lucasff15@hotmail.com>
2023-09-13 23:52:51 -0300

    modified:   src/forum/components/modals/InitialModal.tsx

--- src/forum/components/modals/InitialModal.tsx
+++ src/forum/components/modals/InitialModal.tsx
@@ -11,6 +11,8 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { useDropzone } from 'react-dropzone';
 import { BsFillFileEarmarkImageFill } from 'react-icons/bs';
 import { BiSolidCloudUpload } from 'react-icons/bi';
+import { styled } from '@mui/material/styles';
+import { motion, AnimatePresence } from 'framer-motion';
 
 const formSchema = z.object({
   name: z.string().min(1, 'Server name is required.'),
@@ -20,6 +22,15 @@ const formSchema = z.object({
       message: 'Please upload an image file.',
     }),
 });
+
+const BootstrapDialog = styled(Dialog)(({ theme }) => ({
+  '& .MuiDialogContent-root': {
+    paddingInline: theme.spacing(5),
+  },
+  '& .MuiPaper-root': {
+    maxWidth: '950px',
+  },
+}));
 
 export default function InitialModal() {
   const [open, setOpen] = useState(true);
@@ -66,172 +77,217 @@ export default function InitialModal() {
       <Button variant='outlined' onClick={handleClickOpen}>
         Open dialog
       </Button>
-      <Dialog
-        onClose={handleClose}
-        aria-labelledby='customized-dialog-title'
-        open={open}
-      >
-        <DialogTitle
-          sx={{
-            fontWeight: 'bold',
-            fontSize: '1.5rem',
-            textAlign: 'center',
-            pt: 3,
-            pb: 1,
-          }}
-          id='customized-dialog-title'
+      <AnimatePresence>
+        <BootstrapDialog
+          onClose={handleClose}
+          aria-labelledby='customized-dialog-title'
+          open={open}
         >
-          Customize your server
-        </DialogTitle>
-        <DialogContent>
-          <DialogContentText sx={{ textAlign: 'center' }}>
-            Give your server a personality with a name and an image. You can
-            always change it later
-          </DialogContentText>
-          <FormControl
+          <DialogTitle
             sx={{
-              py: 3,
-              width: '100%',
+              fontWeight: 'bold',
+              fontSize: '1.5rem',
+              textAlign: 'center',
+              pt: 3,
+              pb: 1,
             }}
+            id='customized-dialog-title'
           >
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <Stack
-                sx={{
-                  display: 'flex',
-                  flexDirection: 'column',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                }}
-              >
+            Customize your server
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText sx={{ textAlign: 'center' }}>
+              Give your server a personality with a name and an image. You can
+              always change it later
+            </DialogContentText>
+            <FormControl
+              sx={{
+                py: 3,
+                width: '100%',
+              }}
+            >
+              <form onSubmit={handleSubmit(onSubmit)}>
                 <Stack
                   sx={{
-                    mb: 2,
-                    width: '450px',
-                    transition: 'all 250ms linear',
-                    backgroundImage: `${
-                      isDragActive
-                        ? `url("data:image/svg+xml,%3csvg width='100%25' height='100%25' xmlns='http://www.w3.org/2000/svg'%3e%3crect width='100%25' height='100%25' fill='none' stroke='%23444C727B' stroke-width='4' stroke-dasharray='6%2c 14' stroke-dashoffset='0' stroke-linecap='square'/%3e%3c/svg%3e")`
-                        : `url("data:image/svg+xml,%3csvg width='100%25' height='100%25' xmlns='http://www.w3.org/2000/svg'%3e%3crect width='100%25' height='100%25' fill='none' stroke='%2338383852' stroke-width='4' stroke-dasharray='6%2c 14' stroke-dashoffset='0' stroke-linecap='square'/%3e%3c/svg%3e")`
-                    }`,
-                    '& .draganddrop': {
-                      width: '100%',
-                      py: '20px',
-                      textAlign: 'center',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                  }}
+                >
+                  <Stack
+                    sx={{
+                      mb: 2,
+                      width: '450px',
                       transition: 'all 250ms linear',
-                      backgroundColor: `${isDragActive ? '#444c7214' : ''}`,
-                      cursor: 'pointer',
-                    },
-                  }}
-                  justifyContent={'center'}
-                  alignItems={'center'}
-                >
-                  <div {...getRootProps()} className='draganddrop'>
-                    <input {...getInputProps({ ...register('imageUrl') })} />
-                    <Stack
-                      sx={{
-                        pb: 1,
-                        '& svg': {
-                          width: '50px',
-                          height: '50px',
-                          transition: 'color 250ms linear',
-                          color: isDragActive ? '#444c727b' : '#000088',
-                        },
-                      }}
-                      justifyContent={'center'}
-                      alignItems={'center'}
-                    >
-                      <BsFillFileEarmarkImageFill />
-                    </Stack>
-                    <Typography
-                      variant='h5'
-                      color={isDragActive ? '#444c727b' : 'primary'}
-                      fontWeight={'bold'}
-                      fontSize={19}
-                    >
-                      Server Image
-                    </Typography>
-                    {isDragActive ? (
-                      <Typography sx={{ color: '#222222ab' }}>
-                        Drop the file here...
-                      </Typography>
-                    ) : (
-                      <Typography sx={{ color: '#222222ab' }}>
-                        Drag and drop some files here, click to select files
-                      </Typography>
-                    )}
-
-                    {file && (
-                      <img
-                        src={file.preview}
-                        alt=''
-                        style={{
-                          marginTop: '15px',
-                          width: '100px',
-                          height: '100px',
-                          borderRadius: '50%',
-                        }}
-                      />
-                    )}
-                    <Stack alignItems={'center'}>
-                      <Button
-                        variant='outlined'
-                        startIcon={<BiSolidCloudUpload />}
+                      backgroundImage: `${
+                        isDragActive
+                          ? `url("data:image/svg+xml,%3csvg width='100%25' height='100%25' xmlns='http://www.w3.org/2000/svg'%3e%3crect width='100%25' height='100%25' fill='none' stroke='%23444C727B' stroke-width='4' stroke-dasharray='6%2c 14' stroke-dashoffset='0' stroke-linecap='square'/%3e%3c/svg%3e")`
+                          : `url("data:image/svg+xml,%3csvg width='100%25' height='100%25' xmlns='http://www.w3.org/2000/svg'%3e%3crect width='100%25' height='100%25' fill='none' stroke='%2338383852' stroke-width='4' stroke-dasharray='6%2c 14' stroke-dashoffset='0' stroke-linecap='square'/%3e%3c/svg%3e")`
+                      }`,
+                      '& .draganddrop': {
+                        width: '100%',
+                        py: '20px',
+                        textAlign: 'center',
+                        transition: 'all 250ms linear',
+                        backgroundColor: `${isDragActive ? '#444c7214' : ''}`,
+                        cursor: 'pointer',
+                      },
+                    }}
+                    justifyContent={'center'}
+                    alignItems={'center'}
+                  >
+                    <div {...getRootProps()} className='draganddrop'>
+                      <input {...getInputProps({ ...register('imageUrl') })} />
+                      <Stack
                         sx={{
-                          textTransform: 'unset',
-                          width: '125px',
-                          mt: 2,
-                          color: isDragActive ? '#444c727b' : '',
-                          borderColor: isDragActive ? '#444c727b' : '',
+                          pb: 1,
+                          '& svg': {
+                            width: '50px',
+                            height: '50px',
+                            transition: 'color 250ms linear',
+                            color: isDragActive ? '#444c727b' : '#000088',
+                          },
                         }}
+                        justifyContent={'center'}
+                        alignItems={'center'}
                       >
-                        Upload
-                      </Button>
-                    </Stack>
-                  </div>
+                        <BsFillFileEarmarkImageFill />
+                      </Stack>
+                      <Typography
+                        variant='h5'
+                        color={isDragActive ? '#444c727b' : 'primary'}
+                        fontWeight={'bold'}
+                        fontSize={19}
+                      >
+                        Server Image
+                      </Typography>
+                      {isDragActive ? (
+                        <Typography sx={{ color: '#222222ab' }}>
+                          Drop the file here...
+                        </Typography>
+                      ) : (
+                        <Typography sx={{ color: '#222222ab' }}>
+                          Drag and drop some files here, click to select files
+                        </Typography>
+                      )}
+
+                      {file && (
+                        <motion.div
+                          initial={{ opacity: 0, y: 50 }}
+                          animate={{ opacity: 1, y: 0 }}
+                          exit={{ opacity: 0, y: 50 }}
+                        >
+                          {file && (
+                            <motion.img
+                              src={file.preview}
+                              alt=''
+                              style={{
+                                marginTop: '15px',
+                                width: '100px',
+                                height: '100px',
+                                borderRadius: '50%',
+                              }}
+                              initial={{ opacity: 0, scale: 0.5 }}
+                              animate={{ opacity: 1, scale: 1 }}
+                              exit={{ opacity: 0, scale: 0.5 }}
+                            />
+                          )}
+                        </motion.div>
+                      )}
+                      <Stack alignItems={'center'}>
+                        <Button
+                          variant='outlined'
+                          startIcon={<BiSolidCloudUpload />}
+                          sx={{
+                            textTransform: 'unset',
+                            width: '125px',
+                            mt: 2,
+                            color: isDragActive ? '#444c727b' : '',
+                            borderColor: isDragActive ? '#444c727b' : '',
+                          }}
+                        >
+                          Upload
+                        </Button>
+                      </Stack>
+                    </div>
+                  </Stack>
+                  <motion.div
+                    initial={{ opacity: 0, x: -10 }}
+                    animate={{ opacity: 1, x: 0 }}
+                    exit={{ opacity: 0 }}
+                  >
+                    {formState.errors.imageUrl?.message && (
+                      <motion.div
+                        className='shake'
+                        initial={{ x: 0 }}
+                        animate={{
+                          x: [-10, 10, -10, 10, 0],
+                          transition: { duration: 0.5 },
+                        }} // Shake animation
+                      >
+                        <Typography sx={{ color: 'tomato', fontSize: '.8rem' }}>
+                          {formState.errors.imageUrl?.message}
+                        </Typography>
+                      </motion.div>
+                    )}
+                  </motion.div>
                 </Stack>
-                {formState.errors.imageUrl?.message && (
-                  <Typography sx={{ color: 'tomato', fontSize: '.8rem' }}>
-                    {formState.errors.imageUrl?.message}
-                  </Typography>
-                )}
-              </Stack>
-              <Controller
-                name='name'
-                control={control}
-                render={({ field }) => (
-                  <TextField
-                    autoFocus
-                    margin='dense'
-                    id='name'
-                    label='Server Name'
-                    type='text'
-                    fullWidth
-                    variant='standard'
-                    {...field}
-                  />
-                )}
-              />
-              <Typography sx={{ color: 'tomato', fontSize: '.8rem' }}>
-                {formState.errors.name?.message}
-              </Typography>
-              <Stack sx={{ pt: 4 }}>
-                <Button
-                  type='submit'
-                  variant='contained'
-                  disabled={isLoading}
-                  sx={{
-                    background: '#222',
-                    textTransform: 'unset',
-                    // width: '150px',
-                  }}
+                <Controller
+                  name='name'
+                  control={control}
+                  render={({ field }) => (
+                    <TextField
+                      autoFocus
+                      margin='dense'
+                      id='name'
+                      label='Server Name'
+                      type='text'
+                      fullWidth
+                      variant='standard'
+                      {...field}
+                    />
+                  )}
+                />
+                <motion.div
+                  initial={{ opacity: 0, x: -10 }}
+                  animate={{ opacity: 1, x: 0 }}
+                  exit={{ opacity: 0 }}
                 >
-                  Create
-                </Button>
-              </Stack>
-            </form>
-          </FormControl>
-        </DialogContent>
-      </Dialog>
+                  {formState.errors.name?.message && (
+                    <motion.div
+                      className='shake'
+                      initial={{ x: 0 }}
+                      animate={{
+                        x: [-10, 10, -10, 10, 0],
+                        transition: { duration: 0.5 },
+                      }} // Shake animation
+                    >
+                      <Typography sx={{ color: 'tomato', fontSize: '.8rem' }}>
+                        {formState.errors.name?.message}
+                      </Typography>
+                    </motion.div>
+                  )}
+                </motion.div>
+                <Stack sx={{ pt: 4 }}>
+                  <Button
+                    type='submit'
+                    variant='contained'
+                    disabled={isLoading}
+                    sx={{
+                      background: '#222',
+                      textTransform: 'unset',
+                      // width: '150px',
+                    }}
+                  >
+                    Create
+                  </Button>
+                </Stack>
+              </form>
+            </FormControl>
+          </DialogContent>
+        </BootstrapDialog>
+      </AnimatePresence>
     </div>
   );
 }
